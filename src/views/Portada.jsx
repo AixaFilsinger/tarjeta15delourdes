eaa0b624ea07711f3e032035fce93a24ae0dbe6f
AixaFilsinger <macafil99@gmail.com>
2025-11-17 12:05:52 -0300

    S

--- src/views/Portada.jsx
+++ src/views/Portada.jsx
@@ -5,7 +5,7 @@ import marco from "../assets/marcoini2.png"
 const Portada = () => {
   return (
     <>
-    <section className="inicio">
+    <section className="inicio d-flex flex-column justify-content-center align-items-center">
      <img src={marco} alt="" width={400} className="mt-5"/>
     </section>
     
