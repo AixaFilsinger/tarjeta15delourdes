5029d5021f9809069b5161f6237acaa8d0123556
AixaFilsinger <macafil99@gmail.com>
2025-11-17 23:56:43 -0300

    E

--- src/views/Portada.jsx
+++ src/views/Portada.jsx
@@ -6,7 +6,7 @@ const Portada = () => {
   return (
     <>
     <section className="inicio d-flex flex-column justify-content-center align-items-center">
-     <img src={marco} alt="" width={400} className="mt-5"/>
+     <img src={marco} alt="" width={400} className="mt-5" data-aos="zoom-in"        data-aos-duration="1500"  data-aos-delay="200"/>
     </section>
     
     </>
